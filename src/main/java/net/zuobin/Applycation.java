c2566ee968f8ba88696855b607d379565c57e453
ericzuobin <ericzuobin@qq.com>
2017-07-18 20:44:40 +0800

    test

--- src/main/java/net/zuobin/Applycation.java
+++ src/main/java/net/zuobin/Applycation.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 //@SpringbootApplication 等同于@Configuration,@EnableAutoConfiguration和 @ComponentScan 并具有他们的默认属性值。
 public class Applycation {
-    
 
+    //Test 
     public static void main(String[] args) {
         SpringApplication.run(Applycation.class,args);
     }
